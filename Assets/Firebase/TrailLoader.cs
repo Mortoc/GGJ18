c91f7fbf5ee8098779365d10cbe0305c3d452a6f
Corey <darnisock@gmail.com>
2018-01-27 21:41:27 -0800

    Potential Line rendering for trails that are still audible

--- Assets/Firebase/TrailLoader.cs
+++ Assets/Firebase/TrailLoader.cs
@@ -8,15 +8,17 @@ using Firebase.Unity.Editor;
 using System.Linq;
 
 public class TrailLoader : MonoBehaviour {
-    // TODO cull "expired" trails from DB 
     private int trailExpirationInSeconds = 300; // 5 min (test)
 
     [SerializeField]
     private List<Trail> trailData = new List<Trail>();
 
-    private DateTime toUtcNow(int ts)
+    [SerializeField]
+    private GameObject trailPrefab;
+
+    private DateTime toUtcNow(long ts)
     {
-        return (new DateTime(2018, 1, 26, 0, 0, 0) + new TimeSpan(0, 0, ts));
+        return (new DateTime(2018, 1, 26, 0, 0, 0) + new TimeSpan(0, 0, Convert.ToInt32(ts)));
     }
 
     void Start() {
@@ -48,7 +50,38 @@ public class TrailLoader : MonoBehaviour {
                     trailDatum.path = path;
                     trailData.Add(trailDatum);
                 }
+                InitializeTrails();
             }
         });
+    }
+
+    void InitializeTrails()
+    {
+        Debug.Log("init trails "+trailData.Count);
+        Color prev = new Color(UnityEngine.Random.Range(0f, 1f), UnityEngine.Random.Range(0f, 1f), UnityEngine.Random.Range(0f, 1f));
+        for(var j = 0; j < trailData.Count; ++j)
+        {
+            var trailDatum = trailData[j];
+            var visibleTrailData = trailDatum.path.Where(tp => DateTime.UtcNow <= toUtcNow(tp.ts) + new TimeSpan(0, 0, trailExpirationInSeconds)).ToList();
+            if(visibleTrailData.Count > 0)
+            {
+                var go = Instantiate(trailPrefab);
+                go.name = "Trail_" + trailDatum.playerName;
+                go.transform.parent = transform;
+                var lineRenderer = go.GetComponent<LineRenderer>();
+                lineRenderer.startColor = prev;
+                lineRenderer.positionCount = visibleTrailData.Count;
+                for (var i = 0; i < visibleTrailData.Count; ++i)
+                {
+                    lineRenderer.SetPosition(i, visibleTrailData[i].position);
+                }
+                prev = new Color(UnityEngine.Random.Range(0f, 1f), UnityEngine.Random.Range(0f, 1f), UnityEngine.Random.Range(0f, 1f));
+                lineRenderer.endColor = prev;
+            } else
+            {
+                // CULL HERE
+                Debug.Log("We should delete " + trailDatum.playerName + "'s trail it's tooooo old");
+            }
+        }
     }
 }
